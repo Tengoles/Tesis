e91b358f7a78b2798d9fc6fa9cc997dcf46aacb7
LA_PI <pablis_94@hotmail.com>
2018-03-26 00:34:04 -0300

    cambios

--- ADS1115.py
+++ ADS1115.py
@@ -12,7 +12,7 @@ ADS1x15_CONFIG_MUX_OFFSET      = 12 # = 1100
 # Maping of gain values to config register values.
 ADS1x15_CONFIG_GAIN = {
     2/3: 0x0000,
-    1:   0x0200,
+    1:   0x0200,	
     2:   0x0400,
     4:   0x0600,
     8:   0x0800,
@@ -26,10 +26,10 @@ ADS1115_CONFIG_DR = {
     16:   0x0020,
     32:   0x0040,
     64:   0x0060,
-    128:  0x0080,
+    128:  0x0080,  
     250:  0x00A0,
     475:  0x00C0,
-    860:  0x00E0
+    860:  0x00E0   #lo mas rapido posible para ambos
 }
 ADS1x15_CONFIG_COMP_WINDOW      = 0x0010
 ADS1x15_CONFIG_COMP_ACTIVE_HIGH = 0x0008
@@ -49,29 +49,72 @@ class ADS1115(object):
         if i2c is None:
             import Adafruit_GPIO.I2C as I2C
             i2c = I2C     
-        self._device = i2c.get_i2c_device(address, **kwargs) 
-
-    def _read(self, mux, gain, data_rate, mode):
+        self._device = i2c.get_i2c_device(address, **kwargs)
+         
+    def readA0(self):
+		self._device.writeList(ADS1x15_POINTER_CONFIG, [0b11000011, 0b11100011])
+		#time.sleep(1.0/860+0.0001)
+		#time.sleep(1.0/859.0)
+		#for i in range(0,8000):
+		#	pass
+		result = self._device.readList(ADS1x15_POINTER_CONVERSION, 2)
+		return self._conversion_value(result[1], result[0])
+    
+    def readECG(self):
+		self._device.writeList(ADS1x15_POINTER_CONFIG, [0b11000011, 0b11100011])
+		#time.sleep(1.0/860+0.0001)
+		#time.sleep(1.0/859.0)
+		#for i in range(0,1000):
+		#	pass
+		result = self._device.readList(ADS1x15_POINTER_CONVERSION, 2)
+		return self._conversion_value(result[1], result[0])
+    
+    def readPO(self):
+		self._device.writeList(ADS1x15_POINTER_CONFIG, [0b11110011, 0b11100011])
+		#time.sleep(1.0/860+0.0001)
+		#time.sleep(1.0/859.0)
+		for i in range(0,1000):
+			pass
+		result = self._device.readList(ADS1x15_POINTER_CONVERSION, 2)
+		return self._conversion_value(result[1], result[0])
+    
+    def _conversion_value(self, low, high):
+		# Convert to 16-bit signed value.
+		value = ((high & 0xFF) << 8) | (low & 0xFF)
+		# Check for sign bit and turn into a negative value if set.
+		if value & 0x8000 != 0:
+			value -= 1 << 16
+		return value
+	  
+    def _read(self, mux, data_rate, mode):
         """Perform an ADC read with the provided mux, gain, data_rate, and mode
         values.  Returns the signed integer result of the read.
         """
-        config = ADS1x15_CONFIG_OS_SINGLE  # Go out of power-down mode for conversion. 0x8000 = 1000000000000000
+        config = ADS1x15_CONFIG_OS_SINGLE  # Go out of power-down mode for conversion. config = 1000000000000000
+        
         # Specify mux value.
-        config |= (mux & 0x07) << ADS1x15_CONFIG_MUX_OFFSET 
-        # Validate the passed in gain and then set it in the config.
-        if gain not in ADS1x15_CONFIG_GAIN:
-            raise ValueError('Gain must be one of: 2/3, 1, 2, 4, 8, 16')
-        config |= ADS1x15_CONFIG_GAIN[gain]
-        # Set the mode (continuous or single shot).
-        config |= mode
-        # Get the default data rate if none is specified (default differs between
-        # ADS1015 and ADS1115).
-        if data_rate is None:
-            data_rate = self._data_rate_default()
-        # Set the data rate (this is controlled by the subclass as it differs
-        # between ADS1015 and ADS1115).
-        config |= self._data_rate_config(data_rate)
-        config |= ADS1x15_CONFIG_COMP_QUE_DISABLE  # Disble comparator mode.
+        config |= (mux & 0x07) << ADS1x15_CONFIG_MUX_OFFSET #mux puede valer 0b100 0b101 para leer las entradas 0 1 4 respectivamente
+        #config = 1100 0000 0000 0000 o 1101 0000 0000 0000 o 1111 0000 0000 0000
+        
+        #vamos a usar siempre gain 1 (0x0200)
+        config |= ADS1x15_CONFIG_GAIN[1] 
+        #config = 1100 0010 0000 0000 o 1101 0010 0000 0000 o 1111 0010 0000 0000
+         
+        # Set the mode (continuous or single shot). #ADS1x15_CONFIG_MODE_SINGLE      = 0x0100
+        config |= mode 
+        #config = 1100 0011 0000 0000 o 1100 0011 0000 0000 o 1111 0011 0000 0000
+		
+		#Set data rate
+        config |= data_rate 	#data_rate queremos que sea 1110 0000 (860 kbps) o 1000 0000 (128 kbps) o 1100 0000 (475 kbps)
+        #config = 1100 0011 1110 0000 o 1101 0011 1110 0000 o 1111 0011 1110 0000 (860 kbps)
+        #config = 1100 0011 1100 0000 o 1101 0011 1100 0000 o 1111 0011 1100 0000 (475 kbps)
+        
+        config |= ADS1x15_CONFIG_COMP_QUE_DISABLE  # Disable comparator mode.
+        #config = 1100 0011 1110 0011 o 1101 0011 1110 0011 o 1111 0011 1110 0011 (860 kbps)
+        #config = 1100 0011 1100 0011 o 1101 0011 1100 0011 o 1111 0011 1100 0011 (475 kbps)
+        #config = 0b11000011 0b11100011 o 0b11010011 0b11100011 o 0b11110011 0b11100011 (860 kbps)
+        #config = 0b11000011 0b11000011 o 0b11010011 0b11000011 o 0b11110011 0b11000011 (475 kbps)
+        
         # Send the config value to start the ADC conversion.
         # Explicitly break the 16-bit value down to a big endian pair of bytes.
         self._device.writeList(ADS1x15_POINTER_CONFIG, [(config >> 8) & 0xFF, config & 0xFF])
@@ -81,15 +124,6 @@ class ADS1115(object):
         # Retrieve the result.
         result = self._device.readList(ADS1x15_POINTER_CONVERSION, 2)
         return self._conversion_value(result[1], result[0])
-
-    def read_adc(self, channel, gain=1, data_rate=None):
-        """Read a single ADC channel and return the ADC value as a signed integer
-        result.  Channel must be a value within 0-3.
-        """
-        assert 0 <= channel <= 3, 'Channel must be a value within 0-3!'
-        # Perform a single shot read and set the mux value to the channel plus
-        # the highest bit (bit 3) set.
-        return self._read(channel + 0x04, gain, data_rate, ADS1x15_CONFIG_MODE_SINGLE)
 
     def start_adc(self, channel, gain=1, data_rate=None):
         """Start continuous ADC conversions on the specified channel (0-3). Will
